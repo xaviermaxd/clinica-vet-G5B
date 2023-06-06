dc0f57252621b6875c4ed09e588f845121b3f9cb
VanSC <vanessa.choque@tecsup.edu.pe>
2023-06-06 14:05:05 -0500

    Entidad

--- src/main/java/com/tecsup/petclinic/entities/Vets.java
+++ src/main/java/com/tecsup/petclinic/entities/Vets.java
@@ -1,5 +1,57 @@
 package com.tecsup.petclinic.entities;
 
-public class Vets {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Vets {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name="first_name")
+	private String name;
+	@Column(name="last_name")
+	private String lastname;
+	
+	public Vets() {
+		super();
+	}
+
+	public Vets(Integer id, String name, String lastname) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.lastname = lastname;
+	}
+	
+	public Vets(String name, String lastname) {
+		super();
+		this.name = name;
+		this.lastname = lastname;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 }
